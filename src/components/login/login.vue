19b0e98212f13c6c77212e139c8eaf3ad1ed940e
杨莹 <2663528635@qq.com>
2021-08-03 10:24:42 +0800

    async和await简化代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -6,73 +6,96 @@
       label-width="80px"
       :model="formdata"
     >
-    <h2>用户登录</h2>
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button
-      @click.prevent = 'handlelogin()'
-      class="login-btn" type="primary">登录</el-button>
+      <el-button @click.prevent="handlelogin()" class="login-btn" type="primary"
+        >登录</el-button
+      >
     </el-form>
   </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
-        username: '',
-        password: ''
-      }
-
-    }
+        username: "",
+        password: "",
+      },
+    };
   },
   methods: {
     // 登录请求
-    handlelogin () {
-      this.$http.post('login', this.formdata)
-        .then(res => {
-          // 对象解构赋值
-          const {
-            data,
-            meta: { msg, status }
-          } = res.data
+    async handlelogin() {
+      // 让异步操作的代码 看起来像同步代码
+      // ES7 async+await
+      const res = await this.$http.post("login", this.formdata)
+        // 对象解构赋值
+        const {
+          data,
+          meta: { msg, status },
+        } = res.data;
 
-          if (status === 200) {
-            // 登录成功
-            // 1、跳转home
-            this.$router.push({ name: 'home'})
-            // 2、提示成功
-            this.$message.success(msg)
-          } else {
-            // 不成功
-            // 提示信息
-            this.$message.warning(msg)
-          }
-        })
-    }
-  }
-}
+        if (status === 200) {
+          // 登录成功
+          // 1、跳转home
+          this.$router.push({ name: "home" });
+          // 2、提示成功
+          this.$message.success(msg);
+        } else {
+          // 不成功
+          // 提示信息
+          this.$message.warning(msg);
+        }
+    },
+
+    // handlelogin () {
+    //   // 让异步操作的代码 看起来像同步代码
+    //   this.$http.post('login', this.formdata)
+    //     .then(res => {
+    //       // 对象解构赋值
+    //       const {
+    //         data,
+    //         meta: { msg, status }
+    //       } = res.data
+
+    //       if (status === 200) {
+    //         // 登录成功
+    //         // 1、跳转home
+    //         this.$router.push({ name: 'home'})
+    //         // 2、提示成功
+    //         this.$message.success(msg)
+    //       } else {
+    //         // 不成功
+    //         // 提示信息
+    //         this.$message.warning(msg)
+    //       }
+    //     })
+    // }
+  },
+};
 </script>
 <style scoped>
-.login-wrap{
+.login-wrap {
   height: 100%;
-  background-color:#324152;
+  background-color: #324152;
   /* 使用弹性盒子在页面中垂直水平居中 */
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.login-wrap .login-form{
+.login-wrap .login-form {
   width: 400px;
   background-color: #fff;
   border-radius: 5px;
   padding: 30px;
 }
-.login-wrap .login-btn{
-  width:100%;
+.login-wrap .login-btn {
+  width: 100%;
 }
 </style>
