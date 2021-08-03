121dbb829a5277225cc06097f6424af2bf4dba60
杨莹 <2663528635@qq.com>
2021-08-03 17:09:11 +0800

    用户列表-分页

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -53,14 +53,41 @@
 
       <el-table-column prop="address" label="操作">
         <template>
-          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
-          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
-          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            circle
+          ></el-button>
+          <el-button
+            size="mini"
+            plain
+            type="success"
+            icon="el-icon-check"
+            circle
+          ></el-button>
+          <el-button
+            size="mini"
+            plain
+            type="danger"
+            icon="el-icon-delete"
+            circle
+          ></el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <!-- 4、分页 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2, 4, 6 ,8]"
+      :page-size="2"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </el-card>
 </template>
 <script>
@@ -80,6 +107,19 @@ export default {
     this.getUserList()
   },
   methods: {
+    // 分页相关的方法
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+      this.pagesize = val
+      // this.pagenum = 1
+      this.getUserList()
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+      this.pagenum = val
+      this.getUserList()
+    },
+
     // 获取用户列表的请求
     async getUserList () {
       // 需要授权
--- src/plugins/http.js
+++ src/plugins/http.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const MyHttpServer = {}
 
 MyHttpServer.install = (Vue) => {
-  axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+  axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
   // 4. 添加实例方法
   Vue.prototype.$http = axios
 }
