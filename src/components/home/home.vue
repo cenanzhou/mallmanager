e0ef8fb88f15b8a1fea3f412c08268805a2a5f54
杨莹 <2663528635@qq.com>
2021-08-03 11:43:46 +0800

    引入导航组件-调整样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -5,7 +5,7 @@
         <!-- 注意：24栏 -->
         <el-col :span="4">
           <div class="grid-content bg-purple">
-            <img src="../../assets/logo.png" alt="无法显示图片">
+            <img src="../../assets/logo.png" alt="无法显示图片" />
           </div>
         </el-col>
         <el-col :span="18" class="middle">
@@ -19,7 +19,79 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <!-- 侧边栏导航 -->
+        <el-menu
+          :unique-opened="true">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -41,10 +113,10 @@ export default {};
   background-color: #e9eef3;
 }
 /* 头部样式 */
-.middle{
-  text-align: center
+.middle {
+  text-align: center;
 }
-.loginout{
+.loginout {
   line-height: 60px;
   text-decoration: none;
 }
